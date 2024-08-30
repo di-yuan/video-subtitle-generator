843cd7f375c0cde3a869b67b37bbde7faf4060c1
Daisy <daisy@foundi.info>
2024-08-30 15:56:08 +0800

    try catch openai error

--- video-subtitle-generator/generator.py
+++ video-subtitle-generator/generator.py
@@ -14,7 +14,6 @@ audio = video.audio
 audio_filepath = f'{data_dir}{audio_filename}'
 audio.write_audiofile(audio_filepath)
 
-# split returns a generator of AudioRegion objects
 # min_dur: minimum duration of a valid audio event in seconds
 # max_dur: maximum duration of an event
 # max_silence: maximum duration of tolerated continuous silence within an event
@@ -22,7 +21,7 @@ audio.write_audiofile(audio_filepath)
 audio_regions = auditok.split(
     audio_filepath,
     min_dur=0.2,
-    max_dur=4,
+    max_dur=10,
     max_silence=2,
     energy_threshold=55
 )
@@ -30,22 +29,25 @@ audio_regions = auditok.split(
 subtitles = []
 client = OpenAI()
 for index, region in enumerate(audio_regions):
-    # Regions returned by `split` have 'start' and 'end' metadata fields
+    # regions returned by `split` have 'start' and 'end' metadata fields
     print('Region {index}: {region.meta.start:.3f}s -- {region.meta.end:.3f}s'
           .format(index=index, region=region))
 
     # region's metadata can also be used with the `save` method
-    # (no need to explicitly specify region's object and `format` arguments)
     filename = 'region_{meta.start:.3f}-{meta.end:.3f}.wav'
     filepath = region.save(f'{data_dir}{filename}')
     print('region saved as: {}'.format(filepath))
 
     audio_file = open(filepath, 'rb')
-    transcription = client.audio.transcriptions.create(
-        model='whisper-1',
-        file=audio_file,
-        prompt='only traditional chinese',
-    )
+    try:
+        transcription = client.audio.transcriptions.create(
+            model='whisper-1',
+            file=audio_file,
+            prompt='only traditional chinese',
+        )
+    except Exception as e:
+        print(e)
+        continue
     print(transcription.text)
 
     start_time = datetime.timedelta(seconds=region.meta.start)
