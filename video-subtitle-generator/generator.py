1c08c59b1c9bc5f1ae8eed76d39db7ad3af97945
Daisy <daisy@foundi.info>
2024-08-30 16:40:23 +0800

    add argparse and remove tmp file

--- video-subtitle-generator/generator.py
+++ video-subtitle-generator/generator.py
@@ -1,53 +1,70 @@
 from moviepy.editor import VideoFileClip
 from openai import OpenAI
+import argparse
 import auditok
 import datetime
+import os
 import srt
 
 
-data_dir = 'local_data/'
-video_filename = 'test.MOV'
-audio_filename = 'test.wav'
+parser = argparse.ArgumentParser(description='Generate subtitles for a video')
+parser.add_argument('--data_dir', type=str, default='local_data/')
+parser.add_argument('--video_filename', type=str, default='test.MOV')
+parser.add_argument('--srt_filename', type=str, default='test.srt')
+parser.add_argument('--min_duration', type=float, default=0.5)
+parser.add_argument('--max_duration', type=float, default=6)
+parser.add_argument('--max_silence', type=float, default=2)
+args = parser.parse_args()
+
+data_dir = args.data_dir
+video_filename = args.video_filename
+srt_filename = args.srt_filename
+wav_filename = 'tmp.wav'
 
 video = VideoFileClip(f'{data_dir}{video_filename}')
 audio = video.audio
-audio_filepath = f'{data_dir}{audio_filename}'
-audio.write_audiofile(audio_filepath)
+wav_filepath = f'{data_dir}{wav_filename}'
+audio.write_audiofile(wav_filepath)
+video.close()
 
 # min_dur: minimum duration of a valid audio event in seconds
 # max_dur: maximum duration of an event
 # max_silence: maximum duration of tolerated continuous silence within an event
 # energy_threshold: threshold of detection
 audio_regions = auditok.split(
-    audio_filepath,
-    min_dur=0.2,
-    max_dur=10,
-    max_silence=2,
+    wav_filepath,
+    min_dur=args.min_duration,
+    max_dur=args.max_duration,
+    max_silence=args.max_silence,
     energy_threshold=55
 )
+os.remove(wav_filepath)
 
 subtitles = []
 client = OpenAI()
 for index, region in enumerate(audio_regions):
     # regions returned by `split` have 'start' and 'end' metadata fields
-    print('Region {index}: {region.meta.start:.3f}s -- {region.meta.end:.3f}s'
+    print('region {index}: {region.meta.start:.3f}s -- {region.meta.end:.3f}s'
           .format(index=index, region=region))
 
     # region's metadata can also be used with the `save` method
-    filename = 'region_{meta.start:.3f}-{meta.end:.3f}.wav'
-    filepath = region.save(f'{data_dir}{filename}')
-    print('region saved as: {}'.format(filepath))
+    region_filename = 'region_{meta.start:.3f}-{meta.end:.3f}.wav'
+    region_filepath = region.save(f'{data_dir}{region_filename}')
+    print('region saved as: {}'.format(region_filepath))
 
-    audio_file = open(filepath, 'rb')
+    region_file = open(region_filepath, 'rb')
     try:
         transcription = client.audio.transcriptions.create(
             model='whisper-1',
-            file=audio_file,
+            file=region_file,
             prompt='only traditional chinese',
         )
     except Exception as e:
         print(e)
         continue
+    finally:
+        region_file.close()
+        os.remove(region_filepath)
     print(transcription.text)
 
     start_time = datetime.timedelta(seconds=region.meta.start)
@@ -55,6 +72,5 @@ for index, region in enumerate(audio_regions):
     subtitles.append(srt.Subtitle(index+1, start=start_time, end=end_time,
                                   content=transcription.text))
 
-srt_filename = 'test.srt'
 with open(f'{data_dir}{srt_filename}', 'w') as srt_file:
     srt_file.write(srt.compose(subtitles))
