098d5aa8f4f576df2cc07ab369ae02f1cadda49b
Daniel Yang <daniel@foundi.info>
2024-09-08 14:47:44 +0800

    merge subtitles

--- video-subtitle-generator/full_video_generator.py
+++ video-subtitle-generator/full_video_generator.py
@@ -1,6 +1,11 @@
 from moviepy.editor import VideoFileClip
 from openai import OpenAI
+from srt import parse as parse_srt
+from srt import Subtitle
+from srt import sort_and_reindex
+from srt import compose
 import argparse
+import datetime
 import os
 
 
@@ -10,7 +15,27 @@ def parse_args():
     parser.add_argument('--data_dir', type=str, default='local_data/')
     parser.add_argument('--video_filename', type=str, default='test.MOV')
     parser.add_argument('--srt_filename', type=str, default='test_full.srt')
+    parser.add_argument(
+        '-mt', '--merged_threshold', type=float, default=0.5,
+        help='threshold(seconds) for merging subtitles')
+    parser.add_argument(
+        '-md', '--max_duration', type=float, default=2,
+        help='max duration(seconds) of each subtitle')
+    parser.add_argument(
+        '-ml', '--max_length', type=int, default=17,
+        help='max number of characters of each subtitle')
+    parser.add_argument(
+        '--fake', action='store_true', help='use local srt file')
     return parser.parse_args()
+
+
+def merge_subtitle(start: Subtitle, end: Subtitle) -> Subtitle:
+    return Subtitle(
+        index=start.index,
+        start=start.start,
+        end=end.end,
+        content=start.content + end.content
+    )
 
 
 if __name__ == '__main__':
@@ -19,6 +44,7 @@ if __name__ == '__main__':
     data_dir = args.data_dir
     video_filename = args.video_filename
     srt_filename = args.srt_filename
+    fake_post = args.fake
     wav_filename = 'tmp.wav'
 
     video = VideoFileClip(f'{data_dir}{video_filename}')
@@ -27,21 +53,52 @@ if __name__ == '__main__':
     audio.write_audiofile(wav_filepath)
     video.close()
 
-    wav_file = open(wav_filepath, 'rb')
-    client = OpenAI()
+    if fake_post:  # use local srt file
+        if not os.path.exists(f'{data_dir}{srt_filename}'):
+            raise FileNotFoundError(f'{data_dir}{srt_filename} not found')
+        transcription = open(f'{data_dir}{srt_filename}', 'r').read()
+    else:  # post to OpenAI API
+        wav_file = open(wav_filepath, 'rb')
+        client = OpenAI()
+        try:
+            print('Transcribing...')
+            transcription = client.audio.transcriptions.create(
+                model='whisper-1',
+                file=wav_file,
+                language='zh',
+                response_format='srt'
+            )
+        except Exception as e:
+            print(e)
+        finally:
+            wav_file.close()
+            os.remove(wav_filepath)
 
-    try:
-        transcription = client.audio.transcriptions.create(
-            model='whisper-1',
-            file=wav_file,
-            prompt='min_duration: 0.5, max_duration: 6',
-            language='zh',
-            response_format='srt'
-        )
-        with open(f'{data_dir}{srt_filename}', 'w') as srt_file:
-            srt_file.write(transcription)
-    except Exception as e:
-        print(e)
-    finally:
-        wav_file.close()
-        os.remove(wav_filepath)
+    subtitles = [subtitle for subtitle in parse_srt(transcription)]
+
+    # merge subtitles
+    MAX_SUBTITLE_LENGTH = args.max_length
+    MERGED_THRESHOLD = datetime.timedelta(seconds=args.merged_threshold)
+    MAX_SUBTITLE_DURATION = datetime.timedelta(seconds=args.max_duration)
+
+    merged_subtitles = []
+    current_srt = subtitles[0]
+    for next_srt in subtitles[1:]:
+        if (
+            next_srt.start - current_srt.end < MERGED_THRESHOLD and
+            current_srt.end - current_srt.start < MAX_SUBTITLE_DURATION and
+            len(current_srt.content + next_srt.content) < MAX_SUBTITLE_LENGTH
+        ):
+            current_srt = merge_subtitle(current_srt, next_srt)
+        else:
+            merged_subtitles.append(current_srt)
+            current_srt = next_srt
+    merged_subtitles.append(current_srt)  # the last subtitle
+    sorted_subtitles = sort_and_reindex(merged_subtitles)
+
+    # write to srt file
+    composed_srt = compose(sorted_subtitles)
+    with open(f'{data_dir}{srt_filename}', 'w') as srt_file:
+        srt_file.write(composed_srt)
+        print(composed_srt)
+    print(f'{data_dir}{srt_filename} generated')
